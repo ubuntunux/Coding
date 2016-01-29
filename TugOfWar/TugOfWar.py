2b800fb6f70cac9d2c7f51e8f120ab65169f5a7b
thyoon <ubuntunux@gmail.com>
2016-01-29 13:17:48 +0900

    tug of war

--- TugOfWar/TugOfWar.py
+++ TugOfWar/TugOfWar.py
@@ -2,11 +2,14 @@ def balance(weights, left, right):
     weight = weights.pop()
     # find closed number with weight
     closedValue = min([(999, 0)] + [(abs(weight-x), x) for x in weights])[1]
-    bigger = max(weight, closedValue)
-    smaller = min(weight, closedValue)
-    left = (if left > right min(weight, closedValue))
-    right += max(weight, closedValue)
     weights.remove(closedValue)
+    #print("left, right, weight, closedValue", left, right, weight, closedValue)
+    if left > right:
+        left += min(weight, closedValue)
+        right += max(weight, closedValue)
+    else:
+        left += max(weight, closedValue)
+        right += min(weight, closedValue)
     if len(weights) != 0:
         return balance(weights, left, right)    
     return (left, right) if left < right else (right, left)
@@ -16,9 +19,9 @@ while True:
   count = int(input("Input Count : "))
   array = []
   for x in range(count):
-    array.append(max(1, min(450, int(input("%d. Input weight : " % (x+1))) )))
+    array.append(max(1, min(450, int(input("%d. Input weight : " % (x+1))))))
   # make even length
   if len(array) % 2 == 1:
       array.append(0)
   # show result
-  print(balance(array, 0, 0) + "\n")
+  print("Result : %d %d\n" % balance(array, 0, 0))
