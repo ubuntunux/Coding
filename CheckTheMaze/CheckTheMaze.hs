6e32b7259174bfe8a8ad180cee5bd56d9ad09c2e
ubuntunux <ubuntunux@gmail.com>
2016-11-23 02:27:09 +0900

    haskell

--- CheckTheMaze/CheckTheMaze.hs
+++ CheckTheMaze/CheckTheMaze.hs
@@ -6,7 +6,7 @@ data1 = lines "<     >"
 data2 = lines
     "########\n\
     \#<     #\n\
-    \#  ##  #\n\
+    \#> ##  #\n\
     \#  ##  #\n\
     \#     >#\n\
     \########"
@@ -41,6 +41,17 @@ endPoint = '>'
 checkedList = [()]
 checkPattern = [(0, -1), (-1, 0), (1, 0), (0, 1)]
 
+getEnableCheckPoint datas curPoint checkedList =
+    filter (\(x, y) -> 
+        let
+            sumX = (x + fst curPoint)
+            sumY = (y + snd curPoint)
+        in
+            not (elem (sumX, sumY) checkedList) &&
+            sumY >= 0 && sumY < length (datas) &&
+            sumX >= 0 && sumX < length (datas !! sumY))
+        checkPattern
+        
 findStartPoint datas num
     | index /= Nothing = Just (num, n)
     | length datas <= num = Nothing
@@ -49,4 +60,10 @@ findStartPoint datas num
         index = elemIndex startPoint (datas !! num)
         Just n = index
 
+findEndPoint datas curPoint@(x,y) checkedList = do
+    (pX, pY) <- getEnableCheckPoint datas curPoint checkedList
+    let
+        sX = (x + pX)
+        sY = (y + pY)
+    if (datas !! sY) !! sX == '>' then [(True, (sX, sY))] else [(False, (sX, sY))]
 
